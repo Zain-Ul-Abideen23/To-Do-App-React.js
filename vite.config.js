7249bfe08f5a0099d1309938cb9b607eec650e8e
Zain-Ul-Abideen23 <z.dude023@gmail.com>
2026-02-08 11:15:22 +0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/To-Do-App-React.js/',
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
-        base: "/To-Do-App-React.js",
       },
     }),
   ],
